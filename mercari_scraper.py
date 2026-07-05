6e781c191bf76cde49ff3744f6f77a8f6be0dc8d
kotokatu <117390625+kotokatu@users.noreply.github.com>
2026-07-05 09:35:56 +0300

    fix scraper

--- mercari_scraper.py
+++ mercari_scraper.py
@@ -156,13 +156,15 @@ class MercariScraper:
             driver = self._get_driver()
 
             params = {
-                "keyword": query["keyword"],
                 "status": "on_sale"
             }
 
             # необязательные параметры
             if query.get("category_id"):
                 params["category_id"] = query["category_id"]
+
+            if query.get("keyword"):
+                params["keyword"] = query["keyword"]
 
             if query.get("brand_id"):
                 params["brand_id"] = query["brand_id"]
